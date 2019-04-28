c0fbff038b8715a28838b622817d7745687319c5
utsushiiro <ozeki.haru@gmail.com>
2019-04-28 11:57:14 +0900

    fix db settings

---
+++ docker/initdb.d/01-create-remote-users.sql
@@ -0,0 +1,1 @@
+GRANT ALL PRIVILEGES ON `markdown_notebook_dev`.* TO `markdown_notebook`@`%` IDENTIFIED BY 'password';
--- docker/initdb.d/02-setup-schema.sql
+++ docker/initdb.d/02-setup-schema.sql
@@ -1,5 +1,5 @@
 create table notes (
-    id integer not null auto increment,
+    id integer not null auto_increment,
     title text not null,
     content text not null,
     updated_at timestamp not null default current_timestamp on update current_timestamp,
