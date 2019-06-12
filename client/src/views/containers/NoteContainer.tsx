7c7c796fb785e7230449a5023d30fe8ee0b46614
utsushiiro <ozeki.haru@gmail.com>
2019-06-12 22:09:49 +0900

    update NoteContainer

--- client/src/views/containers/NoteContainer.tsx
+++ client/src/views/containers/NoteContainer.tsx
@@ -12,29 +12,39 @@ import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Paper from "@material-ui/core/Paper";
 import Divider from "@material-ui/core/Divider";
+import {Box} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import Breadcrumbs from "@material-ui/core/Breadcrumbs";
+import HomeIcon from '@material-ui/icons/Home';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      flexGrow: 1
+    path: {
+      padding: theme.spacing(1, 2),
     },
     content: {
-      padding: "20px"
+      flexGrow: 1
     },
     button: {
       margin: theme.spacing(1)
-    }
+    },
+    icon: {
+      width: 20,
+      height: 20,
+      verticalAlign: "text-bottom"
+    },
   })
 );
 
 const editorModeStyle = {
-  height: "100vh",
   position: "absolute",
-  zIndex: 1,
-  width: "100%",
   top: 0,
   left: 0,
-  border: "none"
+  width: "100%",
+  height: "100vh",
+  borderRadius: "unset",
+  zIndex: 1,
+  backgroundColor: "white"
 };
 
 type Props = {
@@ -43,13 +53,25 @@ type Props = {
   isFetching: boolean;
 };
 
+const tabComponentSwitcher = value => {
+  return (
+    <>
+      {value === 0 && <div>Item One</div>}
+      {value === 1 && <div>Item Two</div>}
+      {value === 2 && <div>Item Three</div>}
+      {value === 3 && <div>Item Four</div>}
+      {value === 4 && <div>Item Five</div>}
+    </>
+  );
+};
+
 const NoteContainer: React.FC<Props> = ({ note, onMount, isFetching }) => {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
 
-  function handleChange(event: React.ChangeEvent<{}>, newValue: number) {
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
     setValue(newValue);
-  }
+  };
 
   useEffect(() => {
     onMount();
@@ -60,26 +82,44 @@ const NoteContainer: React.FC<Props> = ({ note, onMount, isFetching }) => {
   return isFetching || note == null ? (
     <div>Loading...</div>
   ) : (
-    <div>
-      {/* <h2>{note.title}</h2> */}
-
-      <Paper className={classes.root} style={s}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-        >
-          <Tab label="View" />
-          <Tab label="Edit" />
-          <Tab label="Others" />
-        </Tabs>
-        <Divider />
-        {value === 0 && <p>Item One</p>}
-        {value === 1 && <p>Item Two</p>}
-        {value === 2 && <p>Item Three</p>}
-      </Paper>
-    </div>
+    <>
+      <Box mt={2}>
+        <Paper className={classes.path}>
+          <Breadcrumbs aria-label="Breadcrumb">
+            <HomeIcon className={classes.icon} />
+            <Typography color="inherit">Path</Typography>
+            <Typography color="inherit">To</Typography>
+            <Typography color="textPrimary">Note</Typography>
+          </Breadcrumbs>
+        </Paper>
+      </Box>
+      <Box mt={2}>
+        <Paper className={classes.content} >
+          <Box p={2}>
+            <Typography variant="h5" component="h1">
+              {note.title}
+            </Typography>
+          </Box>
+          <Divider />
+          <Box style={s}>
+            <Tabs
+                value={value}
+                onChange={handleChange}
+                indicatorColor="primary"
+                textColor="primary"
+            >
+              <Tab label="View" />
+              <Tab label="Edit" />
+              <Tab label="History" />
+              <Tab label="Children" />
+              <Tab label="Others" />
+            </Tabs>
+            <Divider />
+            {tabComponentSwitcher(value)}
+          </Box>
+        </Paper>
+      </Box>
+    </>
   );
 };
 
