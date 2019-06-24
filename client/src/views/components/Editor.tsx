c12e97b9d2f2d4f8c22bf441a834478efcdec5ff
utsushiiro <ozeki.haru@gmail.com>
2019-06-24 21:38:12 +0900

    create footer for editor tab

--- client/src/views/components/Editor.tsx
+++ client/src/views/components/Editor.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import { Note } from "../../state/notes/types";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
+import { Divider, Box, Button } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -11,11 +11,15 @@ const useStyles = makeStyles((theme: Theme) =>
       display: "block",
       boxSizing: "border-box",
       width: "100%", 
-      height: "calc(100vh - 48px - 1px)", 
+      height: "calc(100vh - 48px - 1px - 1px - 52px)", 
       resize: "none",
       padding: "20px",
       border: "none",
       outline: "none",
+    },
+    footer: {
+      display: "flex",
+      flexDirection: "row-reverse"
     }
   })
 );
@@ -39,6 +43,15 @@ const Editor: React.FC<Props> = ({
           onChange={e => setContent(e.currentTarget.value)}
         />
       </form>
+      <Divider />
+        <Box p={1} className={classes.footer}>
+          <Button
+            variant="contained"
+            color="primary"
+          >
+            Update
+          </Button>
+        </Box>
     </div>
   );
 };
