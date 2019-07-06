a97f66974ca17500b371173643560725305ebee1
utsushiiro <ozeki.haru@gmail.com>
2019-07-07 02:06:43 +0900

    create group schema

--- docker/initdb.d/01-setup-schema.sql
+++ docker/initdb.d/01-setup-schema.sql
@@ -1,19 +1,38 @@
 create table user (
-    id integer not null auto_increment,
-    name varchar(255) unique not null,
-    email varchar(255) unique not null,
+    id bigint not null auto_increment,
+    name varchar(255) not null,
+    email varchar(255) not null,
     password char(60) not null,
     role varchar(20) not null,
     updated_at timestamp not null default current_timestamp,
     created_at timestamp not null default current_timestamp,
-    primary key (id)
+    primary key (id),
+    unique (name),
+    unique (email)
+);
+
+create table `group` (
+    id bigint not null auto_increment,
+    name varchar(255) not null,
+    updated_at timestamp not null default current_timestamp,
+    created_at timestamp not null default current_timestamp,
+    primary key (id),
+    unique (name)
+);
+
+create table user_group_relation (
+    user_id bigint not null,
+    group_id bigint not null,
+    unique (user_id, group_id),
+    foreign key (user_id) references user (id),
+    foreign key (group_id) references `group` (id)
 );
 
 create table note (
-    id integer not null auto_increment,
+    id bigint not null auto_increment,
     title text not null,
     content text not null,
-    user_id integer not null,
+    user_id bigint not null,
     updated_at timestamp not null default current_timestamp,
     created_at timestamp not null default current_timestamp,
     primary key (id),
