96bdfe71cabb81102f862d691c8a5a60ff9e0fb0
utsushiiro <ozeki.haru@gmail.com>
2019-07-14 16:39:21 +0900

    force createdAt sync

--- api/src/main/java/jp/utsushiiro/sharenotes/api/domain/Note.java
+++ api/src/main/java/jp/utsushiiro/sharenotes/api/domain/Note.java
@@ -51,6 +51,7 @@ public class Note{
 
     public void initRevision(NoteRevision revision) {
         latestNoteRevisionMapping = new LatestNoteRevisionMapping(this, revision);
+        revision.setCreatedAt(this.createdAt);
     }
 
     @JsonIgnore
