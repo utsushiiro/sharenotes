c9e3f548a13158aeae64a9ddd06dd1c9ab375b08
utsushiiro <ozeki.haru@gmail.com>
2019-07-21 12:49:57 +0900

    impl validation error handler

--- api/src/main/java/jp/utsushiiro/sharenotes/api/dto/form/SignUpForm.java
+++ api/src/main/java/jp/utsushiiro/sharenotes/api/dto/form/SignUpForm.java
@@ -7,12 +7,19 @@ import javax.validation.constraints.Pattern;
 
 @Data
 public class SignUpForm {
-    @Pattern(regexp = "^[a-zA-Z][a-zA-Z0-9]+$")
+    @Pattern(
+            regexp = "^[a-zA-Z][a-zA-Z0-9]+$",
+            message = "Please input using half-width English numbers and letters, using more than 2 characters. " +
+                      "The first letter can not be a number."
+    )
     private String username;
 
-    @Email
+    @Email(message = "Please input a well-formed email address.")
     private String email;
 
-    @Pattern(regexp = "^[a-zA-Z0-9]{8,16}$")
+    @Pattern(
+            regexp = "^[a-zA-Z0-9]{8,16}$",
+            message = "Please input using half-width English numbers and letters, using more than 8 characters but less than 16."
+    )
     private String password;
 }
--- api/src/main/java/jp/utsushiiro/sharenotes/api/error/ApiExceptionHandler.java
+++ api/src/main/java/jp/utsushiiro/sharenotes/api/error/ApiExceptionHandler.java
@@ -1,9 +1,16 @@
 package jp.utsushiiro.sharenotes.api.error;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.MessageSourceResolvable;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -14,6 +21,14 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
  */
 @RestControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private final MessageSource messageSource;
+
+    @Autowired
+    public ApiExceptionHandler(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @Override
     protected ResponseEntity<Object> handleExceptionInternal(
             Exception ex,
@@ -23,7 +38,34 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
             WebRequest request
     ) {
         ex.printStackTrace();
-        return super.handleExceptionInternal(ex, new ApiError(ex), headers, status, request);
+
+        if (body instanceof ApiError) {
+            return super.handleExceptionInternal(ex, ex, headers, status, request);
+        }else {
+            return super.handleExceptionInternal(ex, new ApiError(ex), headers, status, request);
+        }
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request
+    ) {
+
+        ApiError apiError = new ApiError("Validation Error");
+        BindingResult bindingResult = ex.getBindingResult();
+
+        for (ObjectError error: bindingResult.getGlobalErrors()) {
+            apiError.addDetail(error.getObjectName(), messageSource.getMessage(error, request.getLocale()));
+        }
+
+        for (FieldError error: bindingResult.getFieldErrors()) {
+            apiError.addDetail(error.getField(), messageSource.getMessage(error, request.getLocale()));
+        }
+
+        return super.handleExceptionInternal(ex, apiError, headers, status, request);
     }
 
     @ExceptionHandler
