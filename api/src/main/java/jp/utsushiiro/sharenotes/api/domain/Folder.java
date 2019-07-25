7d0d32f719f65e3c2fb461bae786982894210299
utsushiiro <ozeki.haru@gmail.com>
2019-07-25 23:54:02 +0900

    fix type

--- api/src/main/java/jp/utsushiiro/sharenotes/api/domain/Folder.java
+++ api/src/main/java/jp/utsushiiro/sharenotes/api/domain/Folder.java
@@ -20,7 +20,7 @@ public class Folder {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
-    private int id;
+    private Long id;
 
     @Column(name = "name")
     private String name;
