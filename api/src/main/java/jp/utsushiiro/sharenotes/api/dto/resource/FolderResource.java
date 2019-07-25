5dbe9aa5700f9dda342778e47343de683bc7f119
utsushiiro <ozeki.haru@gmail.com>
2019-07-26 08:59:55 +0900

    impl FolderController

---
+++ api/src/main/java/jp/utsushiiro/sharenotes/api/dto/resource/FolderResource.java
@@ -0,0 +1,32 @@
+package jp.utsushiiro.sharenotes.api.dto.resource;
+
+import jp.utsushiiro.sharenotes.api.domain.Folder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+public class FolderResource {
+    private Long id;
+
+    private String name;
+
+    private LocalDateTime updatedAt;
+
+    private String updatedBy;
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+    public FolderResource(Folder folder) {
+        this.id = folder.getId();
+        this.name = folder.getName();
+        this.updatedAt = folder.getUpdatedAt();
+        this.updatedBy = folder.getUpdatedBy().getName();
+        this.createdAt = folder.getCreatedAt();
+        this.createdBy = folder.getCreatedBy().getName();
+    }
+}
