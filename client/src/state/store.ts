ce5d0cf37d29bbb2685d774bd72d4283153dc303
utsushiiro <ozeki.haru@gmail.com>
2019-08-15 10:12:35 +0900

    impl typed useSelector

--- client/src/state/store.ts
+++ client/src/state/store.ts
@@ -6,16 +6,22 @@ import { connectRouter } from "connected-react-router";
 import { routerMiddleware } from "connected-react-router";
 import { createBrowserHistory } from "history";
 import authReducer from "./auth";
+import {
+  useSelector as useReduxSelector,
+  TypedUseSelectorHook,
+} from 'react-redux'
 
 export const history = createBrowserHistory();
 const middlewares = [logger, routerMiddleware(history), thunk];
+const rootReducer = combineReducers({
+  notesState: notesReducer,
+  authState: authReducer,
+  router: connectRouter(history)
+});
+type RootState = ReturnType<typeof rootReducer>;
+export const useSelector: TypedUseSelectorHook<RootState> = useReduxSelector
 
 export default function configureStore(initialState = {}) {
-  const rootReducer = combineReducers({
-    notesState: notesReducer,
-    authState: authReducer,
-    router: connectRouter(history)
-  });
   return createStore(
     rootReducer,
     initialState,
