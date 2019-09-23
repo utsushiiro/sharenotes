57645f9074595c96e1e1629f4fe0211791677e0a
utsushiiro <ozeki.haru@gmail.com>
2019-09-23 23:45:45 +0900

    fix router bug

--- client/src/state/router/hooks.ts
+++ client/src/state/router/hooks.ts
@@ -1,10 +1,11 @@
 import { useDispatch } from "react-redux";
 import { Path, LocationState } from "history";
+import { push as routerPush } from "connected-react-router";
 
 export function useRouter() {
   const dispatch = useDispatch();
   const push = (path: Path, state?: LocationState) => {
-    dispatch(push(path, state));
+    dispatch(routerPush(path, state));
   };
 
   return {
